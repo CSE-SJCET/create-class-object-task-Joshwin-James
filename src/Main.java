cda0b78992ac3cd92235fad4844726405c74eaba
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2025-07-28 13:11:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+class Student {
+    String name;
+    int rollNo;
+    char grade;
 
-    // TODO: Create a method to display student details
+    public void displayDetail() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
+}
 public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Joshwin";
+        student.rollNo = 43;
+        student.grade = 'C';
+
+        student.displayDetail();
+    }
 }
